56cdbce2744a1c8555134ef928f7b007a28d9172
Lucas <luclucsjoqvist@gmail.com>
2018-02-03 01:00:29 +0100

    subtask B done

--- app/resources/GetBooksResource.js
+++ app/resources/GetBooksResource.js
@@ -4,7 +4,11 @@
   var LibraryDAO = require('../dao/LibraryDAO')
   let book = require('../dao/book')
   let book0 = book.book(0, 'The Hitchhikers Guide to the Galaxy', 'Douglas Adams', 'Science Fiction', 100, 1979, 'blablabla')
+  let book1 = book.book(0, 'dasd', 'Douglas Adams', 'Science Fiction', 100, 1979, 'blablabla')
+  JSON.stringify(book0)
+  JSON.stringify(book1)
   module.exports = function (callback, title) { // The title is optional and is only present when searching. (You need yo modify the books.js file first)
     console.log(book0)
+    console.log(book1)
   }
 }())
