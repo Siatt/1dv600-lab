092d6480c68654bf064b4bfe0c786e9b9245631d
Lucas <luclucsjoqvist@gmail.com>
2018-05-17 04:03:02 +0200

    added addbook func

--- app/resources/AddBookResource.js
+++ app/resources/AddBookResource.js
@@ -1,11 +1,31 @@
 (function () {
-    "use strict";
+  'use strict'
 
-    var LibraryDAO = require('../dao/LibraryDAO');
+  var LibraryDAO = require('../dao/LibraryDAO')
 
+  module.exports = function (data, callback) {
+    LibraryDAO.readXMLFile(books => {
+        // Get last book for id
+      let lastBook = books[books.length - 1]
+      let id = parseInt(lastBook.$.id) + 1
 
-    module.exports = function (data, callback) {
+      let bookToAdd = {
+        '$': {id: id},
+        title: data.title,
+        author: data.author,
+        genre: data.genre,
+        price: data.price,
+        published_date: data.published_date,
+        description: data.description
+      }
 
-    };
-
-}());
+      books.push(bookToAdd)
+      let newList = {
+        catalog: {
+          book: books
+        }
+      }
+      LibraryDAO.writeXMLFile(newList, callback)
+    })
+  }
+}())
